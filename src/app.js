0e34eb2568040fbe337f00ac4e5afde877b29538
jms_dcksn <james.dickson@automationanywhere.com>
2020-10-19 09:17:11 -0500

    Added Bot Deploy API call

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const auth = require('./utils/auth')
 const bot = require('./utils/bot')
+const botDeploy = require('./utils/bot-deploy')
 const runAsUser = require('./utils/run-as-user')
 
 const userName = 'james.dickson.bc'
 const apiKey = '57eqPZZ!Iw#4zH'
 const runnerUser = 'james.dickson.runner'
 const crURL = 'http://localhost:80'
-const botName = 'Fleetcor Live Agent UI'
+const botName = 'Msg Box Test'
 
 auth(crURL, userName, apiKey, (error, {token}) => {
     if (error){
@@ -19,13 +20,23 @@ auth(crURL, userName, apiKey, (error, {token}) => {
         }
         console.log('User ID: ' + userId)
         console.log('Device ID: ' + device)
+
+        bot(crURL, token, botName, (error, {botId}={}) => {
+            if (error){
+                return console.log(error)
+            }
+            console.log(botId)
+
+            botDeploy(crURL, token, botId, userId, (error, {deploymentId}={}) => {
+                if(error){
+                    return console.log(error)
+                }
+                console.log(deploymentId)
+            })
+
+        })
     })
-    bot(crURL, token, botName, (error, {botId}={}) => {
-        if (error){
-            return console.log(error)
-        }
-        console.log(botId)
-    })
+    
 })
 
 
